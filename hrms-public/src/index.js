20472ecdfec2a0c3f1bd6a18963d6df03dfb60ea
Yüksel Can Özdemir <yukselcanozdemir@gmail.com>
2021-06-19 12:27:31 +0300

    initial commit
    frontend development started

---
+++ hrms-public/src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import './assets/css/bootstrap.min.css';
+//import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './assets/css/font-awesome.min.css';
+//import '../node_modules/css-loader/dist/cjs.js??ref--5-oneOf-4-1!./node_modules/postcss-loader/src??postcss!./src/assets/css/font-awesome.min.css';
+import './assets/css/style.css';
+ReactDOM.render(
+  /*
+  
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+  */
+ <App/>, document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
---
+++ hrms-public/src/layouts/MainDashboard.jsx
@@ -0,0 +1,4 @@
+import React from 'react'
+/*import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row,Col } from 'react-bootstrap'; */
+
